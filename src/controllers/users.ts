afabd51778f1e9c2f2b9b0c299393c9414a2d9f9
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-21 21:19:41 +0000

    create delete router and functionality

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,12 +1,25 @@
 import express from "express";
 
-import { getUsers } from "../db/users";
+import { deleteUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
         const users = await getUsers();
 
         return res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(400);
+    }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+
+        const deletedUser = await deleteUserById(id);
+
+        return res.json(deletedUser);
     } catch (err) {
         console.log(err);
         return res.sendStatus(400);
